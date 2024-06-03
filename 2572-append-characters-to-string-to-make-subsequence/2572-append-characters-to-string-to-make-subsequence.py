8b021f488b2fc4cc950ae26c32772b6cfac69c31
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-03 00:50:08 -0400

    Time: 62 ms (58.95%), Space: 17.6 MB (72.52%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.py
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.py
@@ -4,12 +4,10 @@ class Solution:
         trackT = 0
         nS = len(s)
         nT = len(t)
-        to_append = 0
         while trackS < nS and trackT < nT:
             if s[trackS] == t[trackT]:
                 trackT += 1
                 trackS += 1
             else:
                 trackS += 1
-        print(trackS, trackT)
         return nT - trackT
