ceab04e13594e66b0e1e0d6e24be236ce34711a1
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-04 00:46:03 -0400

    Time: 31 ms (89.43%), Space: 16.5 MB (80.97%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,17 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        hmap = {}
-        odd = 0
-        res = 0
-        for c in s:
-            hmap[c] = hmap.get(c, 0) + 1
-        for k,v in hmap.items():
-            # print(k, v)
-            if v%2 == 0:
-                print(k, v)
-                res += v
-            if v%2 != 0:
-                odd = 1
-                res += v-1
-        return res + 1 if odd else res
+        ss = set()
+        for i in s:
+            # IF c not in set
+            if i not in ss: 
+                # Push into set
+                ss.add(i)
+                # print(ss)
+            else: 
+                # Remove if same character is encountered again, satisfies palindrome condition
+                ss.remove(i)
+                # print(ss)
+        return len(s) - len(ss) + 1 if len(ss) != 0 else len(s)
+        # hmap = {}
+        # odd = False
+        # res = 0
+        # for c in s:
+        #     hmap[c] = hmap.get(c, 0) + 1
+
+        # for v in hmap.values():
+        #     if v%2 == 0:
+        #         res += v
+        #     if v%2 != 0:
+        #         print(v)
+        #         odd = True
+        #         res += v-1
+        # return res + 1 if odd else res
         
