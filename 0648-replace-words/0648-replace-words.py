4e73d86f48d03e7ebad487af6719feaa72d0cce2
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-07 17:48:50 -0400

    Time: 339 ms (16.81%), Space: 24.8 MB (94.74%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,29 +1,31 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        res = ""
-        dictionary.sort()
-        sentence = [word for word in sentence.split()]
-        for word in sentence:
-            new_root = word
-            temp = len(sentence)
-            flag = True
-            for root in dictionary:
+        rootset = set(dictionary)
 
-                # print('word:', word, 'root:', root)
-                if len(root)>len(word):
-                    continue
-                for i in range(len(root)):
-                    if root[i] != word[i]:
-                        flag = False
-                        break
-                    flag = True
-                # print(len(root), temp)
-                if len(root) < temp and flag:
-                    # print('True', len(root), len(new_root))
-                    new_root = root
-                    temp = len(new_root)
-                    
-                
-            res += new_root + " "
-        
-        return res.strip()
+        def replace(word):
+            for i in range(1, len(word)):
+                if word[:i] in rootset:
+                    return word[:i]
+            return word
+
+        return " ".join(map(replace, sentence.split()))
+        # res = ""
+        # dictionary.sort()
+        # sentence = [word for word in sentence.split()]
+        # for word in sentence:
+        #     new_root = word
+        #     temp = len(sentence)
+        #     flag = True
+        #     for root in dictionary:
+        #         if len(root)>len(word):
+        #             continue
+        #         for i in range(len(root)):
+        #             if root[i] != word[i]:
+        #                 flag = False
+        #                 break
+        #             flag = True
+        #         if len(root) < temp and flag:
+        #             new_root = root
+        #             temp = len(new_root)
+        #     res += new_root + " "
+        # return res.strip()
