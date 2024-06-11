a628bc94b655b176998f256a3a094d92d4b19b8f
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-11 18:12:36 -0400

    Time: 42 ms (50%), Space: 16.7 MB (66.46%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -5,7 +5,6 @@ class Solution:
         uncommon = []
         for num in arr1:
             hmap[num] = hmap.get(num,0) + 1
-        print('hmap:', hmap)
         for num in arr2:
             common += [num]*hmap[num]
             del hmap[num]
@@ -13,5 +12,4 @@ class Solution:
         for num in sorted(hmap):
             common+= [num] * hmap[num] 
 
-        print(common)
         return common
