f1358c5503f9c4b805274a1d3bfc9428093599bf
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-11 18:14:12 -0400

    Time: 33 ms (91.43%), Space: 16.8 MB (29.73%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -2,14 +2,14 @@ class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         hmap = {}
         common = []
-        uncommon = []
+        arr1.sort()
         for num in arr1:
             hmap[num] = hmap.get(num,0) + 1
         for num in arr2:
             common += [num]*hmap[num]
             del hmap[num]
 
-        for num in sorted(hmap):
+        for num in hmap:
             common+= [num] * hmap[num] 
 
         return common
