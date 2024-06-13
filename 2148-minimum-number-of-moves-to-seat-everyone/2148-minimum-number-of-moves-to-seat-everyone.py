dc41be06df7a32aa0778d52531f24c218e2df7d0
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-13 02:28:33 -0400

    Time: 63 ms (33.76%), Space: 16.5 MB (82.54%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.py
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.py
@@ -1,7 +1,13 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        # Sort the arrays
         seats.sort()
         students.sort()
+        # The elements are sorted so for example: seats = [2,2,6,6], students = [1,3,2,6], 
+        # we get -> seats = [2,2,6,6], students = [1,2,3,6]
+        # The seat closest to the corresponding student is assigned
+        # Think of it like getting seated in ith row because you have jth height.
+        # Taller student gets seated at the later rows.
         res = 0
         for i in range(len(seats)):
             res += abs(seats[i] - students[i])
