8d17bb1822d66b3caca22761556be6c453f16ca7
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-25 18:35:00 -0400

    Time: 30 ms (89.24%), Space: 16.6 MB (29.93%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.py
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.py
@@ -7,6 +7,7 @@
 class Solution:
     def bstToGst(self, root: TreeNode) -> TreeNode:
         nodes = []
+        # Normal inorder to get the sequence of nodes in ascending
         def inorder(root):
             if not root:
                 return
@@ -18,17 +19,21 @@ class Solution:
         inorder(root)
         # print(nodes)
         n = len(nodes)
+
+        # Let's create a postfix sum array using the sequence
         postfix = [0 for _ in range(n)]
         postfix[0] = nodes[-1]
         for i in range(n):
             postfix[i] = postfix[i-1] + nodes[n-i-1]
         # print(postfix[::-1])
+
+        # Using another inorder traversal, keep replacing the node value with postfix sum
         def inorder2(root):
             if not root:
                 return
             inorder2(root.left)
             root.val = postfix.pop(-1)
-            # somehow pop the corresponding node
+            # pop the corresponding node which is at the end
             # print(root.val)
             inorder2(root.right)
         inorder2(root)
