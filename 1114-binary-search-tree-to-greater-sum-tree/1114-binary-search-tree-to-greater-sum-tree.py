7f3ebe592437a28e525aa3e9195729842d679475
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-25 18:40:24 -0400

    Time: 35 ms (63.37%), Space: 16.4 MB (74.01%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.py
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.py
@@ -8,34 +8,37 @@ class Solution:
     def bstToGst(self, root: TreeNode) -> TreeNode:
         nodes = []
         # Normal inorder to get the sequence of nodes in ascending
+        self.total = 0
         def inorder(root):
             if not root:
                 return
             # print(root.val)
-            inorder(root.left)
-            nodes.append(root.val)
             inorder(root.right)
+            self.total += root.val
+            # nodes.append(root.val)
+            root.val = self.total
+            inorder(root.left)
         
         inorder(root)
         # print(nodes)
-        n = len(nodes)
+        # n = len(nodes)
 
-        # Let's create a postfix sum array using the sequence
-        postfix = [0 for _ in range(n)]
-        postfix[0] = nodes[-1]
-        for i in range(n):
-            postfix[i] = postfix[i-1] + nodes[n-i-1]
-        # print(postfix[::-1])
+        # # Let's create a postfix sum array using the sequence
+        # postfix = [0 for _ in range(n)]
+        # postfix[0] = nodes[-1]
+        # for i in range(n):
+        #     postfix[i] = postfix[i-1] + nodes[n-i-1]
+        # # print(postfix[::-1])
 
-        # Using another inorder traversal, keep replacing the node value with postfix sum
-        def inorder2(root):
-            if not root:
-                return
-            inorder2(root.left)
-            root.val = postfix.pop(-1)
-            # pop the corresponding node which is at the end
-            # print(root.val)
-            inorder2(root.right)
-        inorder2(root)
+        # # Using another inorder traversal, keep replacing the node value with postfix sum
+        # def inorder2(root):
+        #     if not root:
+        #         return
+        #     inorder2(root.left)
+        #     root.val = postfix.pop(-1)
+        #     # pop the corresponding node which is at the end
+        #     # print(root.val)
+        #     inorder2(root.right)
+        # inorder2(root)
         return root
         
