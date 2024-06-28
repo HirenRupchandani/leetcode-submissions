9f1b26af7ecdbc8960eb00063fca66a33e3da4d1
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-28 17:57:25 -0400

    Time: 1276 ms (43.95%), Space: 45.3 MB (10.89%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
@@ -1,22 +1,13 @@
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
-        def sort_key(key):
-            # print(len(key[1]))
-            return len(key)
-        imp = {}
-        for src, dst in roads:
-            imp[src] = imp.get(src, 0) + 1
-            imp[dst] = imp.get(dst, 0) + 1
-        # print(imp)
-        
-        imps = dict(sorted(imp.items(), key=lambda item: item[1], reverse=True))
-
-        # print(imps)
         result = 0
-        importances = {k:n-i for i,k in enumerate(imps.keys())}
-        # print(importances)
+        importances = {}
         for src, dst in roads:
-            # print(src, dst, importances[src] + importances[dst])
+            importances[src] = importances.get(src, 0) + 1
+            importances[dst] = importances.get(dst, 0) + 1
+        importances = dict(sorted(importances.items(), key=lambda item: item[1], reverse=True))
+        importances = {k:n-i for i,k in enumerate(importances.keys())}
+        # print(importance)
+        for src, dst in roads:
             result += importances[src] + importances[dst]
-
         return result
