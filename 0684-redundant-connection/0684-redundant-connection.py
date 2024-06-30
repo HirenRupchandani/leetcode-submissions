9d0f173907157c4e33dc00ee3b6feee664f9b17a
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-30 16:27:35 -0400

    Time: 56 ms (53.72%), Space: 16.8 MB (94.77%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,16 +1,22 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # Parent of each node
         par = [i for i in range(len(edges) + 1)]
+        
+        # Rank of each node -> More children = More Rank
         rank = [1] * (len(edges) + 1)
 
+        # Check the parent (ancestor) of the node 
         def find(n):
             p = par[n]
             while p!=par[p]:
+                # Jumping to grandparent -> Path Compression
                 par[p] = par[par[p]]
                 p = par[p]
             return p
         
         def union(n1, n2):
+            # Check between parents of n1, and n2
             p1, p2 = find(n1), find(n2)
 
             if p1 == p2:
