09119889be1bed337972bb7470217dff22e5a3ad
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-07-01 01:19:44 -0400

    Time: 45 ms (71.6%), Space: 16.6 MB (93.64%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.py
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.py
@@ -1,16 +1,12 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        flag = False
-        flagCounter = 0
+        odds = 0
         for number in arr:
             if number%2 == 0:
-                flag = False
-                flagCounter = 0
-
+                odds = 0
             else:
-                flag = True
-                flagCounter += 1
-                if flagCounter == 3:
+                odds += 1
+                if odds == 3:
                     return True
         return False
         
