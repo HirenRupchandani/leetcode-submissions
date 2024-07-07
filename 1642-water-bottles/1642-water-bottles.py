88efb67ac0cd53d2454c5a94824ef43ef0835cb9
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-07-07 13:55:18 -0400

    Time: 30 ms (81.25%), Space: 16.5 MB (79.17%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.py
+++ 1642-water-bottles/1642-water-bottles.py
@@ -1,12 +1,15 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        # Time & space: O(1) -> Better Solution (Geometric Sum)
+        return numBottles + (numBottles - 1) // (numExchange - 1)
+
+
+        # Time: O(log(numBottles)), space: O(1) -> My solution
         result = numBottles
-        while numBottles:
+        while numBottles >= numExchange:
             numBottles, rem = numBottles // numExchange, numBottles % numExchange
             result += numBottles
             numBottles += rem
-            if numBottles < numExchange:
-                break
         return result
 
         
