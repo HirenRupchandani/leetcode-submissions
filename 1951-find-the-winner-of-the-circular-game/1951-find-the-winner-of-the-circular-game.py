99cf8addf20efef0a24c865360fcce4cf1b67210
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-07-08 12:38:31 -0400

    Time: 42 ms (47.24%), Space: 16.6 MB (49.47%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
@@ -1,5 +1,13 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
+        # Recursion With Subproblems
+        def recursion(n, k):
+            if n==1:
+                return 0
+            return (recursion(n-1, k) + k) % n
+        return recursion(n, k) + 1
+        
+        # Simulation -> My Solution
         i = 0
         l = [i+1 for i in range(n)]
         while len(l) > 1:
