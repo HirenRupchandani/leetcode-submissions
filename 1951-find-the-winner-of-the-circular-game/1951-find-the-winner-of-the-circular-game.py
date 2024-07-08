ea7624fa07d5404e61602675813918fa514fa737
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-07-08 12:42:55 -0400

    Time: 29 ms (94.72%), Space: 16.5 MB (84.3%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.py
@@ -1,13 +1,19 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        # Recursion With Subproblems
+        # Josephus problem -> T: O(n), S: O(1)
+        index = 0
+        for i in range(1, n+1):
+            index = (index + k) % i
+        return index + 1
+
+        # Recursion With Subproblems -> T: O(n), S: O(n)
         def recursion(n, k):
             if n==1:
                 return 0
             return (recursion(n-1, k) + k) % n
         return recursion(n, k) + 1
         
-        # Simulation -> My Solution
+        # Simulation -> My Solution -> T: O(n^2), S: O(n)
         i = 0
         l = [i+1 for i in range(n)]
         while len(l) > 1:
