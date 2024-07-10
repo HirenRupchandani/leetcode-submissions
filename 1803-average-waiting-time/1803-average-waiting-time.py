4ab99b3574256be9d8a0b4b81cb3589cb3d12488
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-07-09 20:51:58 -0400

    Time: 744 ms (66.67%), Space: 57.1 MB (45.02%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -3,13 +3,14 @@ class Solution:
         waiting = []
         current_time_cooking = 0
         for arrival, prep_time in customers:
-            # If idle -> waiting time is smaller than arrival
+            # If idle -> current_time_cooking is smaller than arrival
             if current_time_cooking < arrival:
                 # Start immediately and add the end time
                 current_time_cooking = prep_time + arrival
-            # If not idle, check the difference in current time and arrival
+            # If not idle, add the prep time to current_time_cooking
             else:
                 current_time_cooking += prep_time
+            # Remove the arrival time for that interval from current_time_cooking
+            # And add to waiting time list
             waiting.append(current_time_cooking - arrival)
         return sum(waiting) / len(customers)
-        
