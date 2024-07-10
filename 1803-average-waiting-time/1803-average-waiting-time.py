adc70c338af0c301cb6cb957ce7bad79025c363b
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-07-09 20:49:27 -0400

    Time: 756 ms (41.92%), Space: 57 MB (72.85%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -1,17 +1,15 @@
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
-        n: int = len(customers)
-        time_waiting: int = customers[0][1]
-        finished_prev = customers[0][0] + customers[0][1]
-
-        for customer_ind in range(1, n, 1):
-            times: list[int] = customers[customer_ind]
-            arrive: int = times[0]
-
-            # chef starts cook this as soon as he finished last dish or customer arrived
-            start_cook: int = max(arrive, finished_prev)
-            end_time: int = start_cook + times[1]
-            finished_prev: int = end_time
-            time_waiting += end_time - arrive
-
-        return time_waiting / n
+        waiting = []
+        current_time_cooking = 0
+        for arrival, prep_time in customers:
+            # If idle -> waiting time is smaller than arrival
+            if current_time_cooking < arrival:
+                # Start immediately and add the end time
+                current_time_cooking = prep_time + arrival
+            # If not idle, check the difference in current time and arrival
+            else:
+                current_time_cooking += prep_time
+            waiting.append(current_time_cooking - arrival)
+        return sum(waiting) / len(customers)
+        
