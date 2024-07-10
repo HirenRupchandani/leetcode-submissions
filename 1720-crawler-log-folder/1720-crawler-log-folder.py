7865545681e13de6455c3ee64ac851c7e42a7a1a
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-07-09 21:11:40 -0400

    Time: 47 ms (72.62%), Space: 16.8 MB (36.19%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.py
+++ 1720-crawler-log-folder/1720-crawler-log-folder.py
@@ -1,14 +1,29 @@
 import re
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        stack = ["main"]
-        pattern = r'([a-zA-Z0-9]+)/'
+        count = 0 # main
         for log in logs:
             if log == "./":
                 continue
             elif log == "../":
+                if count > 0:
+                    count -= 1
+            else:
+                count += 1
+        return count
+                
+        stack = ["main"]
+        # Pattern to find folder name
+        pattern = r'([a-zA-Z0-9]+)/'
+        for log in logs:
+            # To stay in same folder
+            if log == "./":
+                continue
+            # To go back (pop the stack head)
+            elif log == "../":
                 if stack[-1] != "main":
                     stack.pop()
+            # Find and insert folder name in stack
             else:
                 # Search for the pattern in the input string
                 match = re.search(pattern, log)
