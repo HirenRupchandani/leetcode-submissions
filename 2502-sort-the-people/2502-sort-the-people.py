ed3ebe0a8ab36c5d9a944bb3d475031c8dbcf97a
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-07-22 01:03:47 -0400

    Time: 109 ms (42.68%), Space: 17.2 MB (14.55%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -2,7 +2,5 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         n = len(heights)
         heap = [(heights[i], names[i]) for i in range(n)]
-
         heap = sorted(heap, reverse=True)
-        # print(heap)
         return [heap[i][1] for i in range(n)]
