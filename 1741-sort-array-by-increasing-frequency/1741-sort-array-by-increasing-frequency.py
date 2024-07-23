fbe844e89f744743459fecb35c0d483cef2889e5
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-07-23 02:14:08 -0400

    Time: 47 ms (75.86%), Space: 16.6 MB (12.04%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.py
@@ -8,20 +8,27 @@ class Solution:
         for num in nums:
             hmap[num] = hmap.get(num, 0) + 1
         # print(hmap)
+        # Convert the hashmap to value, key pairs where keys are in a list
         for k,v in hmap.items():
             if v not in h2:
                 h2[v] = []
             h2[v].append(k)
         # print(h2)
+        # Sort the keys (frequencies)
         h2 = dict(sorted(h2.items()))
         # print("sorted", h2)
 
+        # For each k, v pair
         for k, v in h2.items():
+            # Check for multiple values
             if len(v) > 1:
+                # If yes, sort the list in descending order
                 v.sort(reverse=True)
+                # Parse every value and add to the result k times
                 for i in range(len(v)):
                     result += [v[i]]*k
             else:
+                # Else just add the value k times
                 result += [v[0]]*k
         # print(result)
         return result
